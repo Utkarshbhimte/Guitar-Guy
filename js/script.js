85210b7a3036b24e0c73ae4eddf2a3c6a9a3de31
UtkarshBhimte <utkarshbhimte95@gmail.com>
2016-10-10 05:05:02 +0530

    side nav working

--- js/script.js
+++ js/script.js
@@ -1,20 +1,25 @@
 var moveOnX = 0;
-$(document).ready(function(){
+$(document).ready(function() {
 
-  var foregroundWidth = $( ".foreground" ).width() / 2;
-  var foregroundHeight = $( ".foreground" ).height() / 2;
+    var foregroundWidth = $(".foreground").width() / 2;
+    var foregroundHeight = $(".foreground").height() / 2;
 
-  $( ".foreground" ).mousemove(function( event ) {
+    $(".foreground").mousemove(function(event) {
+        var pageCoords = "( " + event.pageX + ", " + event.pageY + " )";
 
-  var pageCoords = "( " + event.pageX + ", " + event.pageY + " )";
-  // $( "span:first" ).text( "( event.pageX, event.pageY ) : " + pageCoords );
-  // console.log(pageCoords);
+        moveOnX = (event.pageX - foregroundWidth) / foregroundWidth;
+        moveOnY = (event.pageY - foregroundHeight) / foregroundHeight;
 
-  moveOnX = (event.pageX - foregroundWidth) / foregroundWidth;
-  moveOnY = (event.pageY - foregroundHeight) / foregroundHeight;
+        $(".text").css('transform', 'translateX(' + moveOnX + 'em) rotateY(' + 60 * moveOnX + 'deg) translateY(' + moveOnY + 'em) rotateX(' + -60 * moveOnY / 2 + 'deg)');
+        $(".guitar").css('transform', 'translateX(' + moveOnX * 20 + 'px)');
 
-  $(".text").css('transform', 'translateX('+moveOnX+'em) rotateY('+ 60*moveOnX +'deg) translateY('+moveOnY+'em) rotateX('+ -60*moveOnY/2 +'deg)');
-  $(".guitar").css('transform', 'translateX('+moveOnX*20+'px)');
+    });
 
-});
+    $('.hamburger-icon').on('click', function(event) {
+      event.preventDefault();
+      $('.sidenav').toggleClass('active');
+      $('.hamburger-icon').toggleClass('active');
+    });
+
+
 });
